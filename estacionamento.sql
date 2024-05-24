5d4935dca8b0ee32c03f5bbae8eda0551f19d37b
dansilx <danilosilveira@msn.com>
2024-05-24 01:12:20 -0300

    salvar

--- estacionamento.sql
+++ estacionamento.sql
@@ -42,7 +42,7 @@ INSERT INTO cliente (cpf, nome, dtNasc) VALUES (123456789, 'João Silva', '1980-
 
 INSERT INTO modelo (Desc_2) VALUES ('Modelo X'), ('Modelo Y'), ('Modelo Z'), ('Modelo W');
 
-INSERT INTO veiculo (placa, Modelo_codMOd, Clinte_cpf) VALUES ('ABC-1234', 1, 123456789), ('XYZ-5678', 2, 987654321),('DBI-1010', 3, 111222333), ('ADS-2024', 4, 444555666);
+INSERT INTO veiculo (placa, cor, Modelo_codMod, Clinte_cpf) VALUES ('ABC-1234','azul' 1, 123456789), ('XYZ-5678','prata', 2, 987654321),('DBI-1010', 'verde', 3, 111222333), ('ADS-2024' '', 4, 444555666);
 
 INSERT INTO patio (num, ender, capacidade) VALUES (1, 'Rua das Flores, 123', 50), (2, 'Avenida das Rosas, 456', 100), (3, 'Rua das Margaridas, 789', 150), (4, 'Avenida dos Lírios, 012', 200);
 
@@ -70,14 +70,69 @@ FROM veiculo
 INNER JOIN estaciona ON veiculo.placa = estaciona.Veiculo_placa
 WHERE estaciona.cod = 1;
 
-SELECT veiculo.placa, modelo.Desc_2
-FROM veiculo
-INNER JOIN modelo ON veiculo.Modelo_codMOd = modelo.codMod
-WHERE veiculo.placa >= 2020; 
+
+-- NÃO HÁ COLUNA ANO 
+-- SELECT veiculo.placa, modelo.Desc_2
+-- FROM veiculo
+-- INNER JOIN modelo ON veiculo.Modelo_codMod = modelo.codMod
+-- WHERE veiculo.placa >= 2020; 
 
 SELECT patio.ender, estaciona.dtEntrada, estaciona.dtSaida
 FROM patio
 INNER JOIN estaciona ON patio.num = estaciona.Patio_num
 INNER JOIN veiculo ON estaciona.Veiculo_placa = veiculo.placa
 WHERE veiculo.placa = 'DBI-1010';
+
+SELECT COUNT(*) AS Qauntidade
+FROM veiculo 
+INNER JOIN estaciona ON veiculo.placa = estaciona.Veiculo_placa
+WHERE veiculo.cor = 'verde';
+
+SELECT cliente.cpf, cliente.nome 
+FROM cliente 
+INNER JOIN veiculo ON cliente.cpf = veiculo.Cliente_cpf
+INNER JOIN Modelo ON veiculo.ModelocodMod = modelo.codMod
+WHERE modelo.Desc_2 LIKE 'Corolla%';
+
+SELECT veiculo.placa, estaciona.hsEntrada, estaciona.hsSaida
+FROM veiculo
+INNER JOIN estaciona ON veiculo.placa = estaciona.Veiculo_placa 
+WHERE veiculo.cor = 'azul';
+
+SELECT patio.ender, estaciona.dtEntrada, estaciona.dtSaida
+FROM patio
+INNER JOIN estaciona ON patio.num = estaciona.Patio_num
+INNER JOIN veiculo ON estaciona.Veiculo_placa = veiculo.placa
+WHERE veiculo.placa = 'ADS-2024';
+
+SELECT cliente.nome, cliente.cpf
+FROM cliente 
+INNER JOIN veiculo ON cliente.cpf = veiculo.Cliente_cpf
+INNER JOIN estaciona ON veiculo.placa = estaciona.Veiculo_placa
+WHERE estaciona.cod = 2;
+
+SELECT modelo.Desc_2
+FROM modelo 
+INNER JOIN veiculo ON modelo.codMod = veiculo.Modelo_codMod 
+INNER JOIN estaciona ON veiculo.placa = estaciona.Veoculo_placa
+WHERE estaciona.cod = 10;
+
+SELECT nome 
+FROM cliente
+WHERE MONTH(dtNasc) = MONTH(CURRENT_DATE);
+
+SELECT cliente.nome, cliente.cpf, modelo.Desc_2, veiculo.cor
+FROM cliente
+INNER JOIN veiculo ON cliente.cpf = veiculo.Cliente_cpf
+INNER JOIN modelo ON veiculo.Modelo_codMod = modelo.codMod
+INNER JOIN estaciona ON veiculo.placa = estaciona.Veiculo_placa
+WHERE estaciona.dtSaida IS NULL
+ORDER BY estaciona.dtEntrada ASC;
+
+SELECT patio.ender, COUNT(*) AS Total
+FROM partio
+INNER JOIN estaciona ON patio.num = estaciona.Patio_num
+INNER JOIN veiculo ON estaciona.Veiculo_placa = veiculo.placa
+WHERE estaciona.dtSaida IS NULL
+GROUP BY patio.ender;
 
